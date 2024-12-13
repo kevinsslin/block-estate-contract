79739e1da7c1a7d9547ecda20258d1b07f4931aa
kevinsslin <kk123750964@gmail.com>
2024-12-13 14:01:30 +0800

    refactor: Add console.log statement to Deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,17 +3,17 @@ pragma solidity 0.8.25;
 
 import { Script } from "forge-std/src/Script.sol";
 import { BlockEstateFactory } from "../src/BlockEstateFactory.sol";
+import { console } from "forge-std/src/console.sol";
 
 contract Deploy is Script {
     function run() external returns (BlockEstateFactory) {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-
         vm.startBroadcast(deployerPrivateKey);
 
         BlockEstateFactory factory = new BlockEstateFactory();
+        console.log("Factory deployed at:", address(factory));
 
         vm.stopBroadcast();
-
         return factory;
     }
 }
